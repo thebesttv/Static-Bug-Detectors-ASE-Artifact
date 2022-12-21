355a1f129e74a08d0bfe24520c777071f4b75665
thebesttv <taojuntian@outlook.com>
2022-12-21 20:29:20 +0800

    Use local Maven mirror

--- analyzers/spotbugs/bugswarm/from_host/add-maven-mirror.sh
+++ analyzers/spotbugs/bugswarm/from_host/add-maven-mirror.sh
@@ -1,7 +1,15 @@
 # Use HuaWei Maven mirror
+# sudo sed -i  '/  <mirrors>/a \
+#     <mirror>\
+#       <id>huaweicloud</id>\
+#       <mirrorOf>*</mirrorOf>\
+#       <url>https://repo.huaweicloud.com/repository/maven/</url>\
+#     </mirror>' /usr/local/maven/conf/settings.xml
+
+# Use local Maven mirror
 sudo sed -i  '/  <mirrors>/a \
     <mirror>\
-      <id>huaweicloud</id>\
-      <mirrorOf>*</mirrorOf>\
-      <url>https://repo.huaweicloud.com/repository/maven/</url>\
+      <id>reposilite</id>\
+      <url>http://host.docker.internal:27492/releases/</url>\
+      <mirrorOf>external:*</mirrorOf>\
     </mirror>' /usr/local/maven/conf/settings.xml
