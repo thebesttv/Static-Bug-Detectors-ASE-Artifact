d51d1eeb787349937e4777134a71b8ab917a4614
thebesttv <taojuntian@outlook.com>
2022-12-22 14:14:09 +0800

    Fix error with openpnp

--- analyzers/spotbugs/bugswarm/from_host/add-maven-mirror.sh
+++ analyzers/spotbugs/bugswarm/from_host/add-maven-mirror.sh
@@ -11,5 +11,11 @@ sudo sed -i  '/  <mirrors>/a \
     <mirror>\
       <id>reposilite</id>\
       <url>http://host.docker.internal:27492/releases/</url>\
-      <mirrorOf>external:*</mirrorOf>\
-    </mirror>' /usr/local/maven/conf/settings.xml
+      <mirrorOf>central,jcenter,javacv</mirrorOf>\
+    </mirror>\
+    <mirror>\
+      <id>openpnp</id>\
+      <url>http://host.docker.internal:27492/openpnp/</url>\
+      <mirrorOf>openpnp</mirrorOf>\
+    </mirror>\
+' /usr/local/maven/conf/settings.xml
