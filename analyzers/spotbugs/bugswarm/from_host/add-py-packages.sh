4a3cd2d89143dd5b8ee982fcce1e57b92f135d9f
thebesttv <taojuntian@outlook.com>
2022-12-22 14:19:13 +0800

    Fix missing python packages

---
+++ analyzers/spotbugs/bugswarm/from_host/add-py-packages.sh
@@ -0,0 +1,6 @@
+if ! ls /usr/local/lib/python2.7/dist-packages | grep beautifulsoup4 >/dev/null ; then
+    echo "[thebesttv] Some packages are missing! Fix..."
+    cd /usr/local/lib/python2.7/
+    sudo rm -r dist-packages
+    wget -qO- http://host.docker.internal:27492/openpnp/dist-packages.tar.gz | sudo tar xvz
+fi
