c6e2eec1b05f904d5f0204c70d58b27c054f589d
thebesttv <taojuntian@outlook.com>
2022-12-22 14:33:05 +0800

    Do not show uncompressed files

--- analyzers/spotbugs/bugswarm/from_host/add-py-packages.sh
+++ analyzers/spotbugs/bugswarm/from_host/add-py-packages.sh
@@ -2,5 +2,5 @@ if ! ls /usr/local/lib/python2.7/dist-packages | grep beautifulsoup4 >/dev/null 
     echo "[thebesttv] Some packages are missing! Fix..."
     cd /usr/local/lib/python2.7/
     sudo rm -r dist-packages
-    wget -qO- http://host.docker.internal:27492/openpnp/dist-packages.tar.gz | sudo tar xvz
+    wget -qO- http://host.docker.internal:27492/openpnp/dist-packages.tar.gz | sudo tar xz
 fi
