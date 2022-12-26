4a260f6940e51c2840e85da5deb94ae78ce7b62c
thebesttv <taojuntian@outlook.com>
2022-12-26 18:17:02 +0800

    Use volume and remove on exit

--- analyzers/nullaway/bugswarm/main.py
+++ analyzers/nullaway/bugswarm/main.py
@@ -71,7 +71,8 @@ class NullAwayRunner(ParallelArtifactRunner):
         c = command.split('\n')
         x = [x.strip() for x in c]
         cmd = ' && '.join(x)
-        command_final = 'docker run --add-host=host.docker.internal:host-gateway -v {}:{} -i {}:{} /bin/bash -lc "{}"'.format(HOST_SANDBOX, procutils.CONTAINER_SANDBOX, dockerhub_repo, image_tag, cmd)
+        volume_name = os.path.basename(HOST_SANDBOX)
+        command_final = 'docker run --rm --add-host=host.docker.internal:host-gateway -v {}:{} -i {}:{} /bin/bash -lc "{}"'.format(volume_name, procutils.CONTAINER_SANDBOX, dockerhub_repo, image_tag, cmd)
         process, stdout, stderr, ok = utils.run_command(command_final)
 
 
