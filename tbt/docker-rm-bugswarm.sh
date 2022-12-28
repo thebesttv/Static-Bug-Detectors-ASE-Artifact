3bbdf26085b8f24542effc14bce9212a7285c10e
thebesttv <taojuntian@outlook.com>
2022-12-28 11:20:03 +0800

    Remove Infer Docker container

--- tbt/docker-rm-bugswarm.sh
+++ tbt/docker-rm-bugswarm.sh
@@ -3,6 +3,9 @@
 if docker ps -a | grep bugswarm >/dev/null ; then
     echo 'Removing all BugSwarm Docker containers...'
     docker rm -f $(docker ps -a | grep bugswarm | cut -d' ' -f1)
-else
-    echo 'No BugSwarm Docker container'
+fi
+
+if docker ps -a | grep datomassi/images:infer-jdk8 >/dev/null ; then
+    echo 'Removing Infer Docker containers...'
+    docker rm -f $(docker ps -a | grep datomassi/images:infer-jdk8 | cut -d' ' -f1)
 fi
